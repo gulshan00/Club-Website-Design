da490b7b28dac5f8a3ed6e4710aa7714ac2a92ee
gulshan <gulshankiul810@gmail.com>
2025-12-25 19:39:31 +0530

    chnages done

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,3 @@
-// // App.jsx
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import RecentEvents from './components/RecentEvents';
-// import AboutSection from './components/AboutSection';
-// import Footer from './components/Footer';
-
-
-
-
-// function App() {
-//   return (
-//     <div className="min-h-screen bg-gray-900 text-white">
-//       <Navbar />
-//       <Hero />
-//       <RecentEvents />
-//       <AboutSection />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
